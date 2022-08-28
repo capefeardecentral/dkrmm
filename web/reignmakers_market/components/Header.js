c04c3a3d54faaeb142c1aa54c38fb5167f3fa9ba
Mike Rapanis <info@capefeardecentral.net>
2022-08-28 15:17:48 -0400

    fix nav home link

--- web/reignmakers_market/components/Header.js
+++ web/reignmakers_market/components/Header.js
@@ -4,7 +4,7 @@ export default function Header (props) {
   return (
     <header className="flex shadow-lg items-center align-middle border-b-green-500 border-b-2 text-gray-700 mb-5 py-10">
       <Link href="/">
-        <h1 className="px-5 md:text-3xl font-light">DKRMM.CFD</h1>
+        <a><h1 className="px-5 md:text-3xl font-light">DKRMM.CFD</h1></a>
       </Link>
       <div className="absolute text-xs md:text-lg right-0 font-bold mx-5 uppercase">
         <Link href="/portfolio">portfolio tracker</Link>
